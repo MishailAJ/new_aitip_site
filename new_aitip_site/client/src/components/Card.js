4938f7b3a11f5e5983a77ee1598af5faed030f40
amphyxs <amphyxs@gmail.com>
2022-12-12 19:16:10 +0300

    Documentation for Card component

--- new_aitip_site/client/src/components/Card.js
+++ new_aitip_site/client/src/components/Card.js
@@ -2,10 +2,31 @@ import React from "react";
 import FadingImg from "./FadingImg";
 import "./Card.css";
 
+/** 
+ * Компонент карточки с закруглёнными углами и местом для изображения (опционально).
+ * 
+ * Пример использования:
+ * ```
+     <Card
+        title="Card title"
+        imgSrc={importedImage}
+        imgPos="left"
+        style={{gridColumnStart: 1, gridColumnEnd: 3}}
+    >
+        <p>Card text</p>
+    </Card>
+ * ```
+ * Данный код задаёт карточку с заголовком "Card title", изображением importedImage, расположенным слева ("left"). 
+ * Причём эта карточка при помещении её в CardContainer будет занимать 1 и 2 столбцы грида.
+ * 
+ * Props:
+ * @param {string} title - заголовок карточки (помещается в стилизованный <h1>).
+ * @param {Object | string} imgSrc - изображение, которое помещается в отведённое для него место (либо импортированное, либо путь к нему). Можно не указывать, если imgPos="none".
+ * @param {"left" | "right" | "top" | "bottom"} imgPos - позиция изображения в карточке (top, right, bottom, left, none). imgPos="none" указывает на отсутствие изображения.
+ * @param {Object} style - дополнительные стили. Также здесь указывается столбцы грида, в которых должна размещаться карточка (пока это не реализовано по-другому).  
+*/
 class Card extends React.Component {
-    /*
-        TODO: сделать компонент ColoredText, который будет брать цвет для текста из пропа primaryColor своей карточки
-    */
+    // TODO: сделать компонент ColoredText, который будет брать цвет для текста из пропа primaryColor своей карточки
 
     constructor(props) {
         super(props);
