cbd8456b79931860e6b60c6c9150f34376e9b893
amphyxs <amphyxs@gmail.com>
2022-12-14 18:09:22 +0300

    Now Card can take additional className prop

--- new_aitip_site/client/src/components/Card.js
+++ new_aitip_site/client/src/components/Card.js
@@ -8,25 +8,34 @@ import "./Card.css";
  * Пример использования:
  * ```
      <Card
+        className="FeedbackPage-bigCard"
         title="Card title"
-        imgSrc={importedImage}
+        imgSrc={ImportedImage}
         imgPos="left"
         style={{gridColumnStart: 1, gridColumnEnd: 3}}
     >
         <p>Card text</p>
     </Card>
  * ```
- * Данный код задаёт карточку с заголовком "Card title", изображением importedImage, расположенным слева ("left"). 
- * Причём эта карточка при помещении её в CardContainer будет занимать 1 и 2 столбцы грида.
+ * Данный код задаёт карточку с заголовком "Card title", изображением ImportedImage, расположенным слева ("left"). 
+ * Причём эта карточка при помещении её в CardContainer будет занимать 1 и 2 столбцы грида 
+ * и у неё будет дополнительный CSS-класс `.FeedbackPage-bigCard`.
  * 
  * Props:
  * @param {string} title - заголовок карточки (помещается в стилизованный <h1>).
  * @param {Object | string} imgSrc - изображение, которое помещается в отведённое для него место (либо импортированное, либо путь к нему). Можно не указывать, если imgPos="none".
  * @param {"left" | "right" | "top" | "bottom", "none"} imgPos - позиция изображения в карточке (top, right, bottom, left, none). imgPos="none" указывает на отсутствие изображения.
+ * @param {string} className - CSS-класс для корневого div карточки.  
  * @param {Object} style - дополнительные стили. Также здесь указывается столбцы грида, в которых должна размещаться карточка (пока это не реализовано по-другому).  
 */
 class Card extends React.Component {
     // TODO: сделать компонент ColoredText, который будет брать цвет для текста из пропа primaryColor своей карточки
+
+    // Значени пропов по умолчанию (если они не были переданы)
+    static defaultProps = {
+        className: '',
+        imgPos: 'left',
+    }
 
     constructor(props) {
         super(props);
@@ -37,7 +46,7 @@ class Card extends React.Component {
         // Для каждого расположения (top, right, bottom, left, none) определены особые стили в CSS-классах, оканчивающиеся на эти расположения
         return (
             <div 
-                className={`Card Card-${this.props.imgPos}`} 
+                className={['Card', `Card-${this.props.imgPos}`, `${this.props.className}`].join(' ')} 
                 style={{...this.props.style}}
             >
                 {
